7a96796a46d7e0ef0a0959c54d038b0439bdbb42
Ben <benwillmustafa@gmail.com>
2023-12-09 23:08:35 +0000

    updated comments

--- Discord-meme-spam-bot.py
+++ Discord-meme-spam-bot.py
@@ -2,13 +2,13 @@ import os
 import discord
 from discord.ext import commands
 
-print("Connecting to Discord...")
+print("Connecting to Discord...")   #To see if code is running
 
-intents = discord.Intents.all()
+intents = discord.Intents.all() #Specifying discord intents as it is a requirement
 intents.messages = True
 intents.guilds = True
 intents.dm_messages = True
-bot = commands.Bot(command_prefix="/", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)     #Spesifying Bot command prefix
 
 @bot.event
 async def on_ready():
@@ -19,8 +19,8 @@ async def on_message(message):
     if message.author == bot.user:
         return  # Ignore messages from the bot itself
 
-    if isinstance(message.channel, discord.DMChannel):
-        # If the message is sent in a DM, forward it to a specific channel
+    if isinstance(message.channel, discord.DMChannel):            # If the message is sent in a DM, forward it to a specific channel
+        
         target_channel_id = 1182090592411471972  
         target_channel = bot.get_channel(target_channel_id)
 
@@ -31,7 +31,7 @@ async def on_message(message):
         for attachment in message.attachments:
             await target_channel.send(f"**I recieved an image:**\n{attachment.url}")
 
-    await bot.process_commands(message)
+    await bot.process_commands(message) 
 
 bot.run(os.environToken)
 
